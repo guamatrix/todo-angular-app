e25fac6eee540d4ca8f6519b1c624b8a045f95dd
Juan Bautista Marval Hernandez <juan.marval@globant.com>
2018-06-19 13:45:26 -0300

    refactor scroll directive

--- src/app/shared/scroll.directive.ts
+++ src/app/shared/scroll.directive.ts
@@ -1,41 +1,22 @@
-import { HostListener, Directive, ElementRef } from '@angular/core';
+import { HostListener, Directive } from '@angular/core';
 
 @Directive({
   selector: '[appScroll]'
 })
 export class ScrollDirective {
-  element: ElementRef;
-  isOver = false;
-
-  constructor(el: ElementRef) {
-    this.element = el;
-  }
+  private isOver = false;
 
   @HostListener('mouseleave') mouseLeave() {
-    //  console.log('enter div');
      this.isOver = false;
-    //  this.element.nativeElement.style.overflowY = 'hidden';
-    //  window.scroll();
   }
 
-  // @HostListener('scroll') scrollCustom() {
-    // console.log('scroll div');
-    // window.scroll(null, null);
-//  }
-
   @HostListener('mouseenter') mouseEnter() {
-    // console.log('out div');
     this.isOver = true;
-    // this.element.nativeElement.style.overflowY = 'auto';
-    // this.element.nativeElement.style.zindex = 5000;
-    // window.scroll(null, null);
  }
 
  @HostListener('window:scroll') scroll() {
-    // console.log('noooo');
     if (this.isOver) {
       window.scroll(null, null);
     }
-    // window.scroll(null, null);
  }
 }
